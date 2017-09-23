80bba3f2ed9c0ef6bff593dbe1ac50c8272d14cd
Avinash Ranjan <avinash.ranjan21@gmail.com>
2017-09-23 09:26:53 +0530

    Sample thread code added in Main.java

--- src/com/devschool/Main.java
+++ src/com/devschool/Main.java
@@ -3,6 +3,18 @@ package com.devschool;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+            String name = Thread.currentThread().getName();
+                System.out.println("I am running in thread "+name);
+            }
+        };
+        Thread thread = new Thread(runnable);
+        thread.start();
+
     }
+
+
 }
